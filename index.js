63ab492d81fe3880edea9554da35820512fa9ad6
jason-bohlinger <j.bohlinger3@gmail.com>
2023-10-02 00:32:44 -0700

    Updated to latest RCCar repo

--- index.js
+++ index.js
@@ -2,6 +2,60 @@
 
 let workspace = null;
 
+Blockly.Blocks['run'] = {
+  init: function() {
+    this.appendStatementInput("COMMAND")
+        .setCheck(["MOVE", "TURN"])
+        .appendField("run");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(65);
+    this.setTooltip("Runs a command.");
+  }
+};
+Blockly.Blocks['move'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("move car forward by")
+        .appendField(new Blockly.FieldNumber(10, 1), "DISTANCE") // Default value of 10, minimum value of 1.
+        .appendField("units");
+    this.setPreviousStatement(true, ["MOVE", "TURN"]);
+    this.setNextStatement(true, ["MOVE", "TURN"]);
+    this.setColour(120);
+    this.setTooltip("Moves the car forward by a specified distance.");
+  }
+};
+
+
+Blockly.Blocks['turn'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("turn car")
+        .appendField(new Blockly.FieldDropdown([["left","LEFT"], ["right","RIGHT"]]), "DIRECTION");
+    this.setPreviousStatement(true, ["MOVE", "TURN"]);
+    this.setNextStatement(true, ["MOVE", "TURN"]);
+    this.setColour(290);
+    this.setTooltip("Turns the car left or right.");
+  }
+};
+
+Blockly.JavaScript.forBlock['move'] = function(block) {
+  var distance = block.getFieldValue('DISTANCE');
+  var code = 'forward(' + distance * 5 + ')\n';
+  return code;
+}
+
+Blockly.JavaScript.forBlock['turn'] = function(block) {
+  var direction = block.getFieldValue('DIRECTION');
+  var code;
+  if (direction == 'LEFT') {
+    code = 'left(90)\n';
+  } else {
+    code = 'right(90)\n';
+  }
+  return code;
+}
+
 function start() {
   // Create main workspace.
   workspace = Blockly.inject('blocklyDiv',
